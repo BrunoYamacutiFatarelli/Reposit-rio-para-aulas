c8bfabb33ecd9fd5509b0abf19d9b4c4e816b50b
Bruno Y Fatarelli <brunoyamacutifatarelli@gmail.com>
2024-03-08 11:33:41 -0300

    mais um arquivo devc++

--- Dev C++/categorias de natacao.cpp
+++ Dev C++/categorias de natacao.cpp
@@ -10,13 +10,13 @@ main()
 	if (idade<=8)
 		printf("\nO nadador pertence a Categoria Infantil A");
 	else
-		if (idade>8 && idade<13)
+		if (idade<13)
 			printf("\nO nadador pertence a Categoria Infantil B");
 		else
-			if (idade>13 && idade<18)
+			if (idade<18)
 				printf("\nO nadador pertence a Categoria Juvenil A");
 			else
-				if (idade>18 && idade<21)
+				if (idade<21)
 					printf("\nO nadador pertence a Categoria Juvenil B");
 				else
 					printf("\nO nadador pertence a Categoria Senior");
